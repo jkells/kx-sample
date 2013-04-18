4f5328df9b129a0b05ba48f1b0613a9be1bdd891
Jared Kells <jkells@gmail.com>
2013-04-18 15:33:38 +1000

    Hook it up

---
+++ Sample.Android/InitialActivity.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using KX.Android;
+using Sample.Core;
+
+namespace Sample.Android
+{
+    [Activity(Label = "Sample.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class InitialActivity : KXActivity
+    {
+        public InitialActivity()
+        {
+            SetApplicationType(typeof (SampleApplication));
+        }
+    }
+}
